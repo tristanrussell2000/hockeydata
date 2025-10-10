e3f00afb1b6f980c0d4312fc1931bdbd4a23266d
Tristan Russell <tristanrussell2000@gmail.com>
2025-10-10 01:19:20 -0700

    Add React Router V7 scaffold, remove old plain react scaffold

---
+++ frontend-rr/app/welcome/welcome.tsx
@@ -0,0 +1,13 @@
+export function Welcome() {
+  return (
+    <main className="flex items-end justify-center pt-16 pb-4">
+      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+        <header className="flex flex-col items-center gap-9 ">
+          <h1 className="text-4xl">
+              Ironclaw's Hockey Analytics
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+}
